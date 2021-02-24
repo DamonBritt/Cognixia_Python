b8d2a7c9f58ef23f24f437fbc87be4a78937e1ac
DamonBritt <damonbritt87@yahoo.com>
2021-02-24 11:27:59 -0600

    Made add/subtract and some comments

--- Calculator.py
+++ Calculator.py
@@ -1,33 +1,16 @@
-num1 = int(input("Please enter a number: "))
-num2 = int(input("Please enter another number. NOT ZERO: "))
-
+num1 = 
+num2 = 
 def add(num1, num2):
-    sum1 = num1 + num2
-    return sum1
+    return sum
 
 def sub(num1, num2):
-    dif = num1 - num2
     return dif
 
 def mult(num1, num2):
-    prod = num1 * num2
     return prod
 
 def div(num1, num2):
-    quot = num1 / num2
     return quot
 
 def mod(num1, num2):
-    rem = num1 // num2
     return rem 
-
-sum1 = add(num1, num2)
-dif = sub(num1, num2)
-prod = mult(num1, num2)
-quot = div(num1, num2)
-mod = mod(num1, num2)
-print("The sum of your two numbers is", sum1)
-print("The difference of your two numbers is", dif)
-print("The product of your two numbers is", prod)
-print("The quotient of your two numbers is", quot)
-print("The modulus of your two numbers is", mod)
